56f9e860924eda5e97649a79d93612a5252b9931
Christoffer Winterkvist <christoffer@winterkvist.com>
2014-02-20 23:17:38 +0100

    Moved DefaultKeyBinding to a new repository (https://github.com/zenangst/DefaultKeyBinding)

--- keybindings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-TARGET="$(eval echo ~${SUDO_USER})/Library/KeyBindings"
-if [ ! -d "$TARGET" ]; then
-  mkdir $TARGET
-fi
-cp "$HOME/Library/Mobile Documents/iCloud/Developer/Configuration/OSX-Configuration/Library/KeyBindings/DefaultKeyBinding.dict" $TARGET
